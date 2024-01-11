45d2136691c06d0c98bced99490eaccfb812b0d4
Rishi Joshi <110519406+tangorishi@users.noreply.github.com>
2024-01-12 02:36:40 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,17 +11,7 @@ function Navbar() {
       </svg>
       <span className="ml-3 text-xl">DellAI</span>
     </a>
-    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-      <a className="mr-5 hover:text-white">First Link</a>
-      <a className="mr-5 hover:text-white">Second Link</a>
-      <a className="mr-5 hover:text-white">Third Link</a>
-      <a className="mr-5 hover:text-white">Fourth Link</a>
-    </nav>
-    <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Button
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-        <path d="M5 12h14M12 5l7 7-7 7"></path>
-      </svg>
-    </button>
+   
   </div>
 </header>
        
